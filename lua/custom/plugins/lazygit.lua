42f9eea2acc6d8d8e26c8c44807e186a8991d3a5
ht3aa <ht3aa@gmail.com>
2024-10-24 22:18:37 +0300

    lazygit flash surround

--- lua/custom/plugins/lazygit.lua
+++ lua/custom/plugins/lazygit.lua
@@ -17,5 +17,6 @@ return {
   -- order to load the plugin when the command is run for the first time
   keys = {
     { '<leader>gg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+    { '<leader>gc', '<cmd>LazyGitFilterCurrentFile<cr>', desc = 'get current file commits' },
   },
 }
