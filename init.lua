e82bec1f8288c699c90baa2104d2e49c5f8b5572
hasan.tahseen <hasan.tahseen@ta3leem.dev>
2024-12-07 18:26:20 +0300

    adding nvim colorize and some note

--- init.lua
+++ init.lua
@@ -735,6 +735,7 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         blade = { 'blade-formatter' },
+        -- php = { 'phpcbf', 'pint', 'pretty-php' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
@@ -864,6 +865,7 @@ require('lazy').setup({
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
     --
+
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'folke/tokyonight.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
---
+++ notes.md
@@ -0,0 +1,4 @@
+> [!IMPORTANT] 2024-11-17
+> ERROR handeling and null handeling is important in programming.
+
+
